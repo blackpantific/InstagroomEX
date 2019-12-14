1d02b7482289eca6f6408c174ad31a081515c53a
Alexey Vovchenko <blackpantific@gmail.com>
2019-12-14 12:55:32 +0300

    no message

--- InstagroomEX/InstagroomEX/ViewModels/WelcomeViewModel.cs
+++ InstagroomEX/InstagroomEX/ViewModels/WelcomeViewModel.cs
@@ -66,7 +66,7 @@ namespace InstagroomEX.ViewModels
                     await NavigationService.NavigateAsync("/NavigationPage/MasterTabbedPageView");
                 }
             }
-            //проверкаfffffffffffffffff
+            //проверкаfffffffffffffffffdfgjghjkl
 
             //var user = await _userDataService.GetUserByUsernameAsync(Username); только по ID
             // CurrentUser -> mastertabbedpage
